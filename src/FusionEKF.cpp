2744a061cfc44d4d8869b360852f5675a78a08ec
MariaSkr <maria.skriaghina@gmail.com>
2018-02-01 21:05:44 +0100

    Update FusionEKF.cpp

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -208,12 +208,12 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
             // Radar updates
 
-        if ((x_new(0) != 0) || (x_new(1) != 0)) 
+        if ((ekf_.x_(0) != 0) || (ekf_.x_(1) != 0)) 
         {
             Hj_ = tools.CalculateJacobian(ekf_.x_);
             ekf_.H_ = Hj_;
             ekf_.R_ = R_radar_;
-            ekf_.UpdateEKF(x_new);
+            ekf_.UpdateEKF(ekf_.x_);
             cout << "Update RADAR done!" << endl;
           }
     
@@ -223,7 +223,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     // Laser updates
         ekf_.R_ = R_laser_;
         ekf_.H_ = H_laser_;
-        ekf_.Update(x_new);
+        ekf_.Update(ekf_.x_);
         cout << "Update LIDAR done!" << endl;
   }
 
